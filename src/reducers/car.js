746bede2becfa3ea18b38835ab2d04eed2ea5a3c
PerervaKostya <pererva.kostya@outlook.com>
2019-09-10 18:24:02 +0300

    first commit

---
+++ src/reducers/car.js
@@ -0,0 +1,33 @@
+export default function () {
+    return [
+        {
+            id: 1,
+            model: "Audi",
+            speed: 234.45,
+            weight: 1.4,
+            descr: "",
+            img: ""
+        },{
+            id: 2,
+            model: "BMW",
+            speed: 220,
+            weight: 1.2,
+            descr: "",
+            img: ""
+        },{
+            id: 3,
+            model: "Mazda",
+            speed: 250,
+            weight: 1.5,
+            descr: "",
+            img: ""
+        },{
+            id: 4,
+            model: "Opel",
+            speed: 212.41,
+            weight: 1.2,
+            descr: "",
+            img: ""
+        },
+    ]
+}
